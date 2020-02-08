640871824d849e6061ce8fa49a86131cf8dfebf9
Cedrick_Alegroso <personal.dancedrick@gmail.com>
2020-02-08 20:12:17 +0800

    ++ instructions for Ticket Injection
    for testing only

--- src/app/servicedashboard/servicedashboard.component.ts
+++ src/app/servicedashboard/servicedashboard.component.ts
@@ -13,6 +13,9 @@ import { Ticket } from '../services/ticket.model';
 })
 export class ServicedashboardComponent implements OnInit {
 
+
+  TicketInjectForm: FormGroup; // for testing tickets only!
+
   constructor(
     public service: ServiceService,
     private _formBuilder: FormBuilder,
@@ -30,14 +33,49 @@ export class ServicedashboardComponent implements OnInit {
       }
     });
 
-  
+     this.TicketInjectForm = this._formBuilder.group({
+      ticketNo: ['', Validators.required],
+    });
 
   }
   
   // test only
+
   testtickets(){
    this.service.testTickets();
   }
+
+
+  /* 
+    
+  @gaille
+     
+     Database collection: tickets
+     Method name should be: injecTicket
+     Form here call the method in the service.service.ts 
+
+     example:
+
+        MethodName(value){
+          this.service."functionsname"(value)
+          .then( res => {
+            console.log(res);
+          }, err => {
+            console.log(err);
+          });
+        }
+
+     because this is for testing purpose only prepare some data to be 
+     inject on the database 
+    
+     needed field in the database is: 
+     serviceUid: <- default data should be: YeDfP4taedaXkv8EEDzMwcHk8Rj2
+     ticketNo:  value.ticketNo
+     ticketOwnerUid: <- default data should be: 30I9qlK4OfZbeBGpzljWfHFuFcL2
+
+     
+     
+  */
 
    serviceSignOut() {
      // call the function to sign out the service
