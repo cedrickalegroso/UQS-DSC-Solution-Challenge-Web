03df4e76389237471a4a8b6d3cc92257fb48b7ea
Cedrick Alegroso <42290274+cedrickalegroso@users.noreply.github.com>
2020-04-26 15:38:35 +0800

    final

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,32 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
 const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+const hbs = require('nodemailer-express-handlebars');
 
-<<<<<<< HEAD
 
 
 //var emailTemp = require("./mail1.html");
-=======
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
 var serviceAccount = require("./theuqspermission.json");
 const config = functions.config().firebase
 
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),
    databaseURL: "https://theuqs-52673.firebaseio.com",
-   config: config
+   config: functions.config().firebase
 });
 
+
+
+var transporter = nodemailer.createTransport(smtpTransport({
+   service: 'gmail',
+   host: 'smtp.gmail.com',
+   auth: {
+      user: functions.config().gmail.email,
+      pass: functions.config().gmail.pass
+   }
+}));
 
 
 const express = require('express');
@@ -26,46 +36,435 @@ const cors = require('cors');
 app.use(cors({ origin: true }));
 
 
+// Cancel ticket
+app.post('/api/NotifyUser:uid:refNo', (req, res) => {
 
-// Create Ticket
-app.post('/api/creaticketNew:sid:uid:abb', (req, res) => {
+   try {
+      console.log('============ using updated api [Notify User] ================')
+      console.log('====================  version APRIL 20   =======================')
 
+      let refNo = req.body.refNo // returns ticket refno
+      let user = req.body.uid // returns user
+      let fcmData;
+      let ticketData
+      let userData;
+
+
+      admin.firestore().collection('fcmTokens').doc(user).get().then((doc) => {
+         fcmData = doc.data();
+         return triggerNotitfyFCM(fcmData, refNo)
+
+      })
+         .catch(err => console.log(err))
+
+
+
+      admin.firestore().collection('tickets').doc(refNo).get().then((doc) => {
+         return doc.data()
+      }).then((results) => {
+         ticketData = results
+         if (ticketData.isEmailNotify === 1) {
+            console.log(ticketData)
+            return triggerNotitfyEmail(user, ticketData)
+         } else {
+            console.log('ticket already notified')
+         }
+         return null
+      })
+         .catch(err => console.log(err))
+
+      admin.firestore().collection('tickets').doc(refNo).update({
+         alreadyNotified: 1
+      })
+
+      return res.status(200).send();
+
+   }
+   catch (error) {
+      console.log(error)
+      return res.status(500).send(error);
+   }
+});
+
+function triggerNotitfyFCM(fcmData, refNo) {
+   console.log('triggerNotitfyFCM')
+   admin.firestore().collection('tickets').doc(refNo).get().then((doc) => {
+      var payload = {
+         notification: {
+            body: 'Your ticket ' + doc.data().ticketNo + ' has reached your given trigger'
+         }
+      }
+      admin.messaging().sendToDevice(fcmData.token, payload)
+      return true;
+   }).catch(err => console.log(err))
+
+
+}
+
+function triggerNotitfyEmail(user, ticketData) {
+   console.log('triggerNotitfyEmail')
+   admin.firestore().collection('userCollection').doc(user).get().then((doc) => {
+      const handlebarOptions = {
+         viewEngine: {
+            extName: '.handlebars',
+            partialsDir: './view/',
+            layoutsDir: './view/trigger/',
+         },
+         viewPath: './view/trigger/',
+         extName: '.handlebars',
+      };
+      transporter.use('compile', hbs(handlebarOptions))
+      const mailOptions = {
+         from: `"Theuqsteam", "<no-reply>"`,
+         to: doc.data().email,
+         subject: "BEEPP BOOP",
+         template: 'trigger',
+         context: {
+            username: doc.data().name,
+            ticket: ticketData.ticketNo,
+         }
+      };
+      transporter.sendMail(mailOptions);
+      return null
+   }).catch(err => console.log(err))
+}
+
+function triggerNotitfySMS(userData, refNo) {
+
+   console.log("Trigger SMS notify ");
+
+}
+
+
+// Cancel ticket
+app.post('/api/CancelTicket:refNo', (req, res) => {
+
+   try {
+      console.log('============ using updated api [cancel ticket] ================')
+      console.log('====================  version APRIL 3   =======================')
+
+      let refNo = req.body.refNo // returns ticket refno
+      let unixTimestamp = Math.floor(Date.now() / 100); // unixtimestamp
+      let date = admin.firestore.Timestamp.now().toDate();
+
+      admin.firestore().collection('tickets')
+         .doc(refNo)
+         .update({
+            ticketStatus: 0
+         })
+         .then(() => {
+            admin.firestore().doc(`tickets/${refNo}/timeline/${unixTimestamp}`)
+               .set({
+                  message: date + ' Ticket Canceled '
+               })
+            return res.status(200).send();
+         })
+         .catch(err => console.log(err))
+
+
+
+   }
+   catch (error) {
+      console.log(error)
+      return res.status(500).send(error);
+   }
+});
+
+
+// Cancel ticket
+app.post('/api/sendEmail', (req, res) => {
+
+   try {
+      console.log('============ using updated api [TEST TWILLIO] ================')
+      console.log('====================  version APRIL 3   =======================')
+
+      const sendMail = (callback) => {
+
+         var transporter = nodemailer.createTransport(smtpTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            auth: {
+               user: "theuqs@gmail.com",
+               pass: "solution2019"
+            }
+         }));
+
+         const handlebarOptions = {
+            viewEngine: {
+               extName: '.hbs',
+               partialsDir: './view/',
+               layoutsDir: './view/',
+            },
+            viewPath: './view/',
+            extName: '.handlebars',
+         };
+
+         transporter.use('compile', hbs(handlebarOptions))
+
+
+         const mailOptions = {
+            from: `"Theuqsteam", ""`,
+            to: `personal.dancedrick@gmail.com`,
+            subject: "Ticket Created",
+            template: 'index',
+            context: {
+               username: "Jacob",
+            }
+         };
+
+         transporter.sendMail(mailOptions, callback);
+      }
+
+
+
+      sendMail((err, info) => {
+         if (err) {
+            console.log(err);
+            res.status(400);
+            res.send({ error: "Failed to send email" });
+         } else {
+            console.log("Email has been sent");
+            res.send(info);
+         }
+      });
+
+      return res.status(200).send()
+
+   }
+   catch (error) {
+      console.log(error)
+      return res.status(500).send(error);
+   }
+});
+
+
+// Read Timeline
+app.get('/api/timeline/:refNo', (req, res) => {
    (async () => {
-
-
       try {
-         console.log('=================== using updated api ========================')
-         console.log('===================  version APRIL 2.1 ========================')
+         console.log('============ using updated api [read timeline] ================')
+         console.log('====================  version APRIL 5   =======================')
+         let refNo = req.params.refNo // returns the service uid
+         const document = admin.firestore().collection('tickets').doc(refNo)
+            .collection('timeline')
+         let timeline = await document.get()
+         let response = timeline.docs.map(doc => doc.data());
 
-         let abbreviation = req.body.abb // returns service abb
-         let serviceUid = req.body.sid // returns the service uid
-         let ticketOwnner = req.body.uid // returns the user uid
+         console.log(response);
+
+         return res.status(200).send(response)
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+
+      }
+   })();
+});
 
 
 
-         admin.firestore().collection('tickets')
-            .where('serviceUid', '==', serviceUid)
-            .where('ticketOwnerUid', '==', ticketOwnner)
-            .where('ticketStatus', '==', 1)
-            .get()
-            .then(snapshot => {
-               if (snapshot.empty) {
-                  console.log('no ticket found for this user ' + ticketOwnner)
-                  return getserviceData(serviceUid, ticketOwnner);
-               } else {
-                  console.log('this user already have a ticket')
-                  message = "Error you already have a existing ticket to this service"
-                  return prepareMessage(message);
+// Find latest done ticket
+app.get('/api/latestTicketDone/:sid', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [read timeline] ================')
+         console.log('====================  version APRIL 5   =======================')
+         let sid = req.params.sid // returns the service uid
+         const document = admin.firestore().collection('tickets')
+            .where('serviceUid', '==', sid)
+            .where('ticketStatus', '==', 2)
+            .orderBy('teller', 'asc')
+            .limit(8)
+         let ticket = await document.get()
+         let response = ticket.docs.map(doc => doc.data());
+
+         console.log(sid)
+         console.log(response)
+
+         return res.status(200).send(response)
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+
+      }
+   })();
+});
+
+
+// Get univ service
+app.get('/api/getUniversity/', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [get services] ================')
+         console.log('====================  version APRIL 9   =======================')
+         const document = admin.firestore().collection('services').where('categoryIndex', '==', 0)
+         let timeline = await document.get()
+         let response = timeline.docs.map(doc => doc.data());
+
+         console.log(response);
+
+         return res.status(200).send(response)
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+
+      }
+   })();
+});
+
+
+// Get univ service
+app.get('/api/getBanks/', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [get services] ================')
+         console.log('====================  version APRIL 9   =======================')
+         const document = admin.firestore().collection('services').where('categoryIndex', '==', 2)
+         let timeline = await document.get()
+         let response = timeline.docs.map(doc => doc.data());
+
+         console.log(response);
+
+         return res.status(200).send(response)
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+
+      }
+   })();
+});
+
+
+
+// Get univ service
+app.get('/api/getGovernment/', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [get services] ================')
+         console.log('====================  version APRIL 9   =======================')
+         const document = admin.firestore().collection('services').where('categoryIndex', '==', 1)
+         let timeline = await document.get()
+         let response = timeline.docs.map(doc => doc.data());
+
+         console.log(response);
+
+         return res.status(200).send(response)
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+
+      }
+   })();
+});
+
+
+
+// new teller notify
+app.post('/api/updateDisplayName:uid:displayName', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [Update DisplayName] ================')
+         console.log('====================  version APRIL 17   =======================')
+         let displayname = req.body.displayName
+         let uid = req.body.uid
+
+         admin.firestore().collection('userCollection').doc(uid).update({
+            displayName: displayname
+         });
+
+         return res.status(200).send()
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+      }
+   })();
+});
+
+
+// new teller notify
+app.post('/api/updatePhoneNumber:uid:phoneNumber', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [Update DisplayName] ================')
+         console.log('====================  version APRIL 17   =======================')
+         let phoneNumber = req.body.phoneNumber
+         let uid = req.body.uid
+
+         admin.firestore().collection('userCollection').doc(uid).update({
+            phoneNumber: phoneNumber
+         });
+
+         return res.status(200).send()
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+      }
+   })();
+});
+
+
+// new teller notify
+app.post('/api/updateprofilepic:uid:fileUrl', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [Update Profile Picture] ================')
+         console.log('====================  version APRIL 17   =======================')
+         let fileUrl = req.body.fileUrl
+         let uid = req.body.uid
+
+         admin.firestore().collection('userCollection').doc(uid).update({
+            photoUrl: fileUrl
+         });
+
+         return res.status(200).send()
+      }
+      catch (error) {
+         console.log(error);
+         return res.status(500).send(error)
+      }
+   })();
+});
+
+
+
+
+
+
+// new teller notify
+app.post('/api/newTellerNotify:uid:teller', (req, res) => {
+   (async () => {
+      try {
+         console.log('============ using updated api [Notify New teller] ================')
+         console.log('====================  version APRIL 5   =======================')
+         let teller = req.body.teller
+         let uid = req.body.uid
+
+         admin.firestore().collection('fcmTokens').doc(uid)
+            .get().then((doc) => {
+               var payload = {
+                  notification: {
+                     body: 'Teller ' + teller + ' is now your teller.'
+                  }
                }
+               admin.messaging().sendToDevice(doc.data().token, payload)
+               return true;
             })
             .catch(err => console.log(err))
 
-            return res.status(200).send();
 
+         return res.status(200).send()
       }
       catch (error) {
-         console.log(error)
-         return res.status(500).send(error);
+         console.log(error);
+         return res.status(500).send(error)
+
       }
    })();
 });
@@ -88,9 +487,10 @@ app.post('/api/notifyNext:refNo', (req, res) => {
             })
             .catch(err => console.log(err))
 
-            admin.firestore().collection("tickets").doc(ticket.refNo).collection('timeline').doc(unixTimestamp).set({
-               message: unixTimestamp + " Ticket " + refNo + " done. "
+            admin.firestore().collection("tickets").doc(refNo).collection('timeline').doc(unixTimestamp).set({
+               message: unixTimestamp + " Ticket " + refNo + " created. "
             });
+      
 
          return res.status(200).send()
       }
@@ -227,31 +627,99 @@ exports.ticketCreated = functions.firestore
          console.log("Error getting document:", error);
       });
 
-
-async function sendMessage(registrationToken, message) {
-   console.log('token is ' + registrationToken)
-   console.log('message is ' + message)
-   var payload = {
-      notification: {
-         title: 'Success',
-         body: message
-      }
-   };
-
-   admin.messaging().sendToDevice(registrationToken, payload)
-      .then(function (response) {
-         console.log("Successfully sent message:", response);
-         console.log(response.results[0].error);
-         return;
-      })
-      .catch(function (error) {
-         console.log("Error sending message:", error);
-         return res.status(500).send();
+      ServiceRef.get().then(function (doc) {
+         if (doc.exists) {
+            ServiceData = doc.data()
+            return sendTicketEmail(ServiceData, ticket);
+         } else {
+            console.log("No such document!");
+            return false;
+         }
+      }).catch(function (error) {
+         console.log("Error getting document:", error);
       });
 
+   });
+
+function sendTicketEmail(ServiceData, ticket) {
+   var UserRef = admin.firestore().collection("userCollection").doc(ticket.ticketOwnerUid);
+   UserRef.get().then(function (doc) {
+      if (doc.exists) {
+         console.log(ServiceData.displayName)
+         console.log(ticket.refNo)
+         console.log(doc.data().email)
+         console.log(ticket.isEmailNotify)
+         console.log(ticket.trigger)
+         console.log(ticket.timestamp)
+
+         const handlebarOptions = {
+            viewEngine: {
+               extName: '.handlebars',
+               partialsDir: './view/',
+               layoutsDir: './view/ticket/',
+            },
+            viewPath: './view/ticket/',
+            extName: '.handlebars',
+         };
+
+         transporter.use('compile', hbs(handlebarOptions))
+         const mailOptions = {
+            from: `"Theuqsteam", "<no-reply>"`,
+            to: doc.data().email,
+            subject: "Ticket Created",
+            template: 'main',
+            context: {
+               displayName: ServiceData.displayName,
+               abbre: ServiceData.abbreviation,
+               ticketNo: ticket.ticketNo,
+               timestamp: ticket.timestamp,
+            }
+         };
+         transporter.sendMail(mailOptions);
+
+         return true;
+
+      } else {
+         // doc.data() will be undefined in this case
+         console.log("No such document!");
+         return false;
+      }
+   }).catch(function (error) {
+      console.log("Error getting document:", error);
+   });
+
+
+
 }
+
+// Mock Data
+app.post('/api/mock', (req, res) => {
+
+   admin.firestore().collection('services').doc('X4TB4qodCcSeE8tG0SbRqSyEPpc2').set({
+      'displayName': 'Banco De oro',
+      'abbreviation': 'BDO',
+      'uid': 'X4TB4qodCcSeE8tG0SbRqSyEPpc2'
+   });
+
+
+   admin.firestore().collection('userCollection').doc('Q3BtesHWliarX1Ig5G2elm0Qlda2').set({
+      'name': 'Jacob',
+      'email': 'personal.dancedrick@gmail.com',
+      'uid': 'Q3BtesHWliarX1Ig5G2elm0Qlda2'
+   });
+
+
+   admin.firestore().collection('fcmTokens').doc('Q3BtesHWliarX1Ig5G2elm0Qlda2').set({
+      'token': 'dj5QwyTiR-yTi1W9SngpxD:APA91bGDJR-c-2eN_sAyJ0dPH6n9_C02UzsU2QgHNBZZC0y9DxMA-4pNVuIVzTAevggacUq-_eqiMfOkXj8UTuOgAm9LQajwwX3YrMZ1EWj0KVdlzIWBUxled0FH65YnLiyFrPHM-AGS',
+   });
+
+
+   return res.status(200).send();
+
+});
 
 
 
 // Export the api to firebse Cloud Functions
 exports.app = functions.https.onRequest(app);
+
