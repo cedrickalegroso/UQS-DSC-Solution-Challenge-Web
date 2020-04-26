b69ea51e2c06eb763fa4089d5375518d6766bced
Cedrick Alegroso <42290274+cedrickalegroso@users.noreply.github.com>
2020-04-26 15:44:16 +0800

    deleted test

--- src/app/services/tickets.service.ts
+++ src/app/services/tickets.service.ts
@@ -6,22 +6,14 @@ import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import * as firebase from 'firebase/app';
-<<<<<<< HEAD
-
-=======
-import 'firebase/firestore';
-import { Component, OnInit } from '@angular/core';
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketsService {
-<<<<<<< HEAD
   readonly ROOT_URL_notifyNewTeller = 'https://us-central1-theuqs-52673.cloudfunctions.net/app/api/newTellerNotify:uid:teller';
   readonly ROOT_URL_notifyNext = 'https://us-central1-theuqs-52673.cloudfunctions.net/app/api/notifyNext:refNo';
-=======
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
   private TicketsCollection: AngularFirestoreCollection<Ticket>;
   tickets$: Observable<Ticket[]>;
   latestTicket$;
@@ -32,14 +24,9 @@ export class TicketsService {
     private afAuth: AngularFireAuth,
     private afs: AngularFirestore,
     private router: Router,
-<<<<<<< HEAD
     private http: HttpClient,
 
   ) {
-=======
-  )
-  { 
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
 
     /* Tickets Status meanings 
       0 - cancelled
@@ -85,19 +72,8 @@ export class TicketsService {
 
   }
 
-<<<<<<< HEAD
   // function to call when ticket is Done
   async ticketDone(ticket) {
-=======
-  ngOnInit() {
-   
-
-
-
-  }
-
-  async ticketDone(ticket){
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
     let ref;
     let unixTimestamp = Math.floor(Date.now() / 1000);
     let ticketColl = this.afs.collection('tickets', ref => ref.where('refNo', '==', ticket.refNo));
@@ -106,7 +82,6 @@ export class TicketsService {
         if (querySnaphot.empty) {
           console.log("ERROR: Could not find ticket");
         } else {
-<<<<<<< HEAD
           querySnaphot.forEach(async function (doc) {
             ref = doc.data().refNo;
             ticketColl.doc(`${ref}`).update({
@@ -119,14 +94,6 @@ export class TicketsService {
             this.http.post(this.ROOT_URL_notifyNext, ticketData).toPromise().then(data => {
               console.log(data)
             });
-=======
-          querySnaphot.forEach( async function(doc) {
-         /* ref = doc.data().refNo;
-          ticketColl.doc(`${ref}`).update({
-          ticketStatus: 0,
-          timestampDone: unixTimestamp
-          });   */ 
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
           });
         }
       }
@@ -148,7 +115,6 @@ export class TicketsService {
         if (querySnaphot.empty) {
           // err
         } else {
-<<<<<<< HEAD
           querySnaphot.forEach(async function (doc) {
             targetUid = doc.data().ticketOwnerUid;
             targetRaw = doc.data().ticketRaw;
@@ -167,31 +133,6 @@ export class TicketsService {
               timestamp: unixTimestamp
             });
           });
-=======
-         querySnaphot.forEach( async function(doc) {
-           targetUid = doc.data().ticketOwnerUid;
-           targetRaw = doc.data().ticketRaw; 
-           let test = doc.data().ticketNo;
-
-
-        
-         let diff = target - ticket.ticketRaw ;
-         let notifRef =  targetUid +  unixTimestamp;
-         let finalMessage = "There are " + diff   + " person(s) before your turn. Please stay at the vicinity of the area.";
- 
-          console.log(test);
-          console.log(finalMessage);
-           notifColl.doc(`${notifRef}`).set({
-           message: finalMessage,
-           notifOwnerUid: 'VuE9JFHoriRD717k14PlW48uAcE2',
-           notifService: service.uid,
-           timestamp: unixTimestamp
-         });  
- 
-        
-
-         });
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
         }
       }
     )
@@ -200,7 +141,6 @@ export class TicketsService {
 
   }
 
-<<<<<<< HEAD
   // resets the ticket Count 
   async ticketCountReset() {
     let service = firebase.auth().currentUser;
@@ -211,11 +151,6 @@ export class TicketsService {
 
   // finds the next ticket
   async nextTicket(selected) {
-=======
-
-  async nextTicket(selected){
-    let data;
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
     let service = firebase.auth().currentUser
 
 
@@ -225,18 +160,10 @@ export class TicketsService {
         if (querySnaphot.empty) {
           console.log("EMPTY");
         } else {
-<<<<<<< HEAD
           querySnaphot.forEach(async function (doc) {
             let data = doc.data()
             ticketColl.doc(`${data.refNo}`).update({
               teller: parseInt(selected),
-=======
-          querySnaphot.forEach( async function(doc) {
-            data = doc.data();
-            console.log(data.ticketNo);
-            ticketColl.doc(`${data.refNo}`).update({
-              teller: selected,
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
               ticketStatus: 2
             });
           });
@@ -260,93 +187,6 @@ export class TicketsService {
         }
       })
     );
-<<<<<<< HEAD
-=======
-  }  
-
-
-  async autoIdTicket() {
-    let data;
-    let unixTimestamp = Math.floor(Date.now() / 1000);
-    let service = firebase.auth().currentUser;
-    let ticketColl = this.afs.collection('tickets', ref => ref.orderBy('ticketRaw', 'desc').where('serviceUid', '==', service.uid).limit(1))
-    ticketColl.get().toPromise().then(
-      function(querySnaphot) {
-      
-        
-         if (querySnaphot.empty) {
-          console.log("empty")
-          ticketColl.doc(`${ 1 + unixTimestamp }`).set({
-            refNo: 'TEST',
-            serviceUid: service.uid,
-            ticketNo: 'TEST' + 1,
-            ticketRaw: 1,
-            ticketOwnerUid: 'TEST',
-            timestamp:  Math.floor(Date.now() / 1000)
-          });
-         } else {
-          querySnaphot.forEach( async function(doc) {
-            data = doc.data();
-            let ticketRaw1 = data.ticketRaw + 1;
-            let ticketOwnerUid = "15VTijHFqoTgmXowy38BlFjPJJ43"
-            let refNo ='TEST' + ticketRaw1 + unixTimestamp;
-            let ticketFinal = 'TEST' + ticketRaw1;
-
-            ticketColl.doc(`${refNo}`).set({
-              refNo: refNo,
-              serviceUid: service.uid,
-              ticketNo: ticketFinal,
-              ticketRaw: ticketRaw1,  
-              ticketOwnerUid: ticketOwnerUid,
-              timestamp:  Math.floor(Date.now() / 1000)
-            });
-          }
-          )
-         }
-        }
-      /*  querySnaphot.forEach( async function(doc) {
-          data = doc.data();
-          console.log(data)
-          if (doc.exists) {
-            data = doc.data();
-            
-            let ticketRaw1 = data.ticketRaw + 1;
-            let unixTimestamp = Math.floor(Date.now() / 1000);
-            let ticketOwnerUid = "15VTijHFqoTgmXowy38BlFjPJJ43"
-            let refNo ='TEST' + ticketRaw1 + unixTimestamp;
-            let ticketFinal = 'TEST' + ticketRaw1;
-
-            if (data.ticketRaw >= 1  ) {    
-              console.log(114)         
-              ticketColl.doc(`${refNo}`).set({
-                  refNo: refNo,
-                  serviceUid: service.uid,
-                  ticketNo: ticketFinal,
-                  ticketRaw: ticketRaw1,  
-                  ticketOwnerUid: ticketOwnerUid,
-                  timestamp:  Math.floor(Date.now() / 1000)
-                });
-            } else {
-              console.log(115)
-              ticketColl.doc(`${refNo}`).set({
-                refNo: refNo,
-                serviceUid: service.uid,
-                ticketNo: 'TEST' + 1,
-                ticketRaw: 1,
-                ticketOwnerUid: ticketOwnerUid,
-                timestamp:  Math.floor(Date.now() / 1000)
-              });
-            }
-
-
-          } else {
-            console.log("Error getting ticket");
-          } 
-        }); */
-     
-    ) 
-  }   
->>>>>>> 87e5d626e5f71eb8dacc5c690fa4b514fa363493
 
 
   }
